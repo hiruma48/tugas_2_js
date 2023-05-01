62564e85b304d442495a8a38be554e23b196c8bf
tri ariyanto <triyanto129@gmail.com>
2023-05-01 14:57:09 +0700

    tugas 2 js
    mengumpulkan tugas 2 js

---
+++ tugas2.js/index.js
@@ -0,0 +1,18 @@
+var number = 20 ;
+var string = "satu,dua,tiga";
+var kosong;
+kosong2 = null;
+var array = [1,2,3,4,5];
+var cm = 180;
+
+ if(cm>150)
+ boolaen = true;
+ else
+ boolaen = false;
+
+console.log(number)
+console.log(string)
+console.log(boolaen)
+console.log(kosong2)
+console.log(kosong)
+console.log(array[2])
